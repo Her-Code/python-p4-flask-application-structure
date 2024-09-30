5dac39e359e04de7adcb5adecb6a2d0933ea0764
+Sharon <+wanjiku.kahira@gmail.com>
2024-09-30 18:54:20 +0300

    Learn flask methods

--- server/app.py
+++ server/app.py
@@ -3,3 +3,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+url = '/<string:username>'
+
+url = url.replace('/<', '')
+
+url = url.replace('>', '')
+
+type, parameter = url.split(':')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
